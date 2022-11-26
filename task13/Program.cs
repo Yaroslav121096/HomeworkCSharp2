a4620049ba076b2021598ba30f6dd42b395c69ba
Yaroslav <Valso96@mail.ru>
2022-11-27 05:01:18 +0600

    Нахождение третьей цифры числа

---
+++ task13/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int rev = 0;
+while (n > 0) 
+{
+    rev = rev * 10 + n % 10;
+    n /= 10;
+}
+if (rev < 99)
+    Console.WriteLine("Третьей цифры нет!");
+else
+{
+    rev = rev % 1000;
+    rev = rev / 100;
+    Console.WriteLine(rev);
+}
