95a0c62115591c23cf00798cfc08ff79a538f7f7
Yaroslav121096 <118292782+Yaroslav121096@users.noreply.github.com>
2022-11-28 12:05:23 +0600

    Update Program.cs

--- task13/Program.cs
+++ task13/Program.cs
@@ -1,17 +1,15 @@
-﻿Console.Clear();
+Console.Clear();
 Console.WriteLine("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 int rev = 0;
-while (n > 0) 
+if (n <= 99)
+    Console.WriteLine("Третьей цифры нет!");
+while (n > 0)
 {
     rev = rev * 10 + n % 10;
     n /= 10;
 }
-if (rev <= 99)
-    Console.WriteLine("Третьей цифры нет!");
-else
-{
+if (rev > 0)
     rev = rev % 1000;
     rev = rev / 100;
     Console.WriteLine(rev);
-}
